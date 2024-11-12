45d0c602813af538df131aac7d9c8d1216eb091f
odai0962 <odai.zagha962@mail.ru>
2024-11-12 04:04:17 +0300

    push

--- src/app/menu.ts
+++ src/app/menu.ts
@@ -1,8 +1,8 @@
 
 
 export const Menu = [
-    { Text: 'menu.products', path: '/home/productsList', role: ['Admin'] },
-    { Text: 'menu.orders', path: '/home/orderList', role: ['Admin'] },
-    { Text: 'menu.warehouse', path: '/home/warehouseList', role: ['Admin'] },
+    { Text: 'menu.products', path: '/home/productsList', role: ['Employee'] },
+    { Text: 'menu.orders', path: '/home/orderList', role: ['Employee'] },
+    { Text: 'menu.warehouse', path: '/home/warehouseList', role: ['Manager'] },
     { Text: 'menu.users', path: '/home/userList', role: ['Admin'] }
 ];
